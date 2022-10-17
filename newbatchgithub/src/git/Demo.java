c781114433aa16bd0a29cc87aea8897a787177be
Jaydeep <Jaydeep@DESKTOP-HBF9AR3>
2022-10-17 16:28:50 +0530

    By priya

--- newbatchgithub/src/git/Demo.java
+++ newbatchgithub/src/git/Demo.java
@@ -5,7 +5,11 @@ public class Demo {
 		System.out.println("welcome git");
 	}
 	public void m1(){
-	System.out.println("push1")
+	System.out.println("push1");
+	}
+	
+	public void m2() {
+		System.out.println("comit 2");
 	}
 
 }
